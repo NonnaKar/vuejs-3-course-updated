700a8d6f3b93633bfa3b76a77ff2543312410f7e
nonna <ukr.nonna@gmail.com>
2021-10-27 20:44:30 -0400

    created computed property

--- section-2/app.js
+++ section-2/app.js
@@ -2,16 +2,37 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
-  methods: {
-    add() {
-      this.counter++
+  computed: {
+    fullname() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Lisova";
     },
-    reduce() {
-      this.counter--
-    }
-  }
+  },
+  methods: {
+    outputFullname() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Lisova";
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
